70ab08cf5906563222cb1bf6e7203c5e65c083e9
ThreadSurfer <giladij@icloud.com>
2022-07-07 23:17:19 -0700

    added toggle mode between different appartments so they show and hide respective side form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
-import { render } from '@testing-library/react';
-import InfoMenu from './components/InfoMenu'
+import InfoMenu from './components/InfoMenu';
 
 class App extends Component {
 
- state = {
-  info_menu_visible: false
- }
+  constructor() {
+    super()
+    this.state = {
+      info_menu_visible: "hidden",
+      selectedApartment: null,
+    }
+    this.displayMenu = this.displayMenu.bind(this)
+  }
 
+ displayMenu = (id) => {
+
+  console.log(id)
+  
+  if(this.state.info_menu_visible == 'hidden'){
+    this.setState({info_menu_visible: 'visible', selectedApartment: id})
+
+  }
+
+  else if(this.state.selectedApartment === id) {
+    this.setState({info_menu_visible: 'hidden'})
+  }
+
+  else {
+    this.setState({selectedApartment: id,})
+  }
+  
+ }
 
   render() {
     return (
       <div className="App">
         <div className="building">
-          <div className='apartment' onClick={(this.sendEmail)} id='1'></div>
-          <div className='apartment' onClick={() => console.log(2)} id='2'></div>
-          <div className='apartment' onClick={() => console.log(3)} id='3'></div>
-          <div className='apartment' onClick={() => console.log(4)} id='4'></div>
-          <div className='apartment' onClick={() => console.log(5)} id='5'></div>
-          <div className='apartment' onClick={() => console.log(6)} id='6'></div>
+          <div className='apartment' onClick={() => this.displayMenu(1)} id='1'></div>
+          <div className='apartment' onClick={() => this.displayMenu(2)} id='2'></div>
+          <div className='apartment' onClick={() => this.displayMenu(3)} id='3'></div>
+          <div className='apartment' onClick={() => this.displayMenu(4)} id='4'></div>
+          <div className='apartment' onClick={() => this.displayMenu(5)} id='5'></div>
+          <div className='apartment' onClick={() => this.displayMenu(6)} id='6'></div>
         </div>
-        <InfoMenu visible="false" />
+       <InfoMenu visibility={this.state.info_menu_visible} selectedApartment = {this.state.selectedApartment} id = {this.state.selectedApartment}/>
       </div>
     );
   }
--- src/components/InfoMenu.js
+++ src/components/InfoMenu.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function InfoMenu() {
+export default function InfoMenu(props) {
+
+    const [visibility, setVisibility] = useState(props.visibility);
+
   return (
     <>
-        <form>
+        <form className='apartmentForm' style = {{visibility: props.visibility}}>
+            <p>{props.visibility}</p>
+            <p>{props.id}</p>
             <label>Test</label>
             <input name="test" type="text"></input>
         </form>
