06acdf528e32384bd2e75c9bc931ad131f42bdf9
ThreadSurfer <giladij@icloud.com>
2022-07-07 21:09:06 -0700

    added form to infomenu

---
+++ src/components/InfoMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function InfoMenu() {
+  return (
+    <>
+        <form>
+            <label>Test</label>
+            <input name="test" type="text"></input>
+        </form>
+    </>
+  )
+}
